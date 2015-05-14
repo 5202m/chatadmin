cb08186ce18f334e93e2ef8db9ca48466d237b96
gavin <gavin@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2015-05-14 06:42:54 +0000

    聊天室bug修复
    git-svn-id: svn://172.30.3.30/pm/pm_mis/trunk@503 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- src/main/webapp/jsp/chat/adminChat.js
+++ src/main/webapp/jsp/chat/adminChat.js
@@ -17,26 +17,34 @@ var adminChat = {
 	 * @param groupName	聊天室name
 	 */
 	add : function(groupId,groupName){
-		$('#'+groupId).linkbutton('disable');
-		var p = $('<div/>').appendTo('body');
-		p.panel({
-			title : groupName,
-			content:'<iframe src="' + $("#chatURL").val()+'&groupId='+ groupId
-					+ '" frameborder=0 height=100% width=100% scrolling=no></iframe>',
-			height:500,
-			closable:true,
-			collapsible:true,
-			onClose : function(){
-				$('#'+groupId).linkbutton('enable');
-				adminChat.pannelCount --;
+		goldOfficeUtils.ajax({
+			url : basePath +'/adminChatController/getToken.do',
+			type : 'get',
+			success : function(data){
+				if(data.obj != null){
+					$('#'+groupId).linkbutton('disable');
+					var p = $('<div/>').appendTo('body');
+					p.panel({
+						title : groupName,
+						content:'<iframe src="' + $("#chatURL").val()+'&groupId='+ groupId+"&token="+data.obj
+								+ '" frameborder=0 height=100% width=100% scrolling=no></iframe>',
+						height:500,
+						closable:true,
+						collapsible:true,
+						onClose : function(){
+							$('#'+groupId).linkbutton('enable');
+							adminChat.pannelCount --;
+						}
+					});
+					$('#pp').portal('add', {
+						panel : p,
+						columnIndex: adminChat.pannelCount%2
+					});
+					$('#pp').portal('resize');
+					adminChat.pannelCount ++;
+				}
 			}
-		});
-		$('#pp').portal('add', {
-			panel : p,
-			columnIndex: adminChat.pannelCount%2
-		});
-		$('#pp').portal('resize');
-		adminChat.pannelCount ++;
+		})
 	}
 };
 
