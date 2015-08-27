3db2f0f805d31f657aa9504eb5b4d43b13bdf71a
alan <alan@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2015-08-27 08:52:48 +0000

    调整代码
    git-svn-id: svn://172.30.3.30/pm/pm_mis/branch/pm_mis_chat@1325 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- src/main/java/com/gwghk/mis/common/controller/UploadController.java
+++ src/main/java/com/gwghk/mis/common/controller/UploadController.java
@@ -214,7 +214,7 @@ public class UploadController extends BaseController{
 			if(json!=null){
 				result.put("state", "SUCCESS");
 				System.out.println("url:"+json.getObj());
-				result.put("url",PropertiesUtil.getInstance().getProperty("pmfilesDomain")+"/"+json.getObj());
+				result.put("url",json.getObj());
 			}
 		} catch (Exception e) {
 			result.put("state", "FAIL");
