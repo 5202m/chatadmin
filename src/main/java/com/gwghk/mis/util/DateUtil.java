b0d9bbab3e9a09207275e5f1768d62f69f4d54b4
dick <dick@53c53a5b-bfa6-924e-aae2-cbdbf9aa2596>
2015-09-17 09:35:24 +0000

    调整代码
    git-svn-id: svn://172.30.3.30/pm/pm_mis/branch/pm_mis_chat@1646 53c53a5b-bfa6-924e-aae2-cbdbf9aa2596

--- src/main/java/com/gwghk/mis/util/DateUtil.java
+++ src/main/java/com/gwghk/mis/util/DateUtil.java
@@ -1687,29 +1687,31 @@ public class DateUtil {
 			}
 			if(dateWeekTimeJSON.containsKey("weekTime")){
 				JSONArray timeArrTmp = dateWeekTimeJSON.getJSONArray("weekTime");
-				Iterator<Object> timeArrIterator = timeArrTmp.iterator();
-				JSONObject timpTmp = null;
-				String timeStartTmp = null;
-				String timeWeekTmp = null;
-				String timeEndTmp = null;
-				while (timeArrIterator.hasNext()) {
-					timpTmp = (JSONObject)timeArrIterator.next();
-
-					loc_result.append(" ");
-					timeWeekTmp = timpTmp.getString("week");
-					if (StringUtils.isNotBlank(timeWeekTmp)) {
-						loc_result.append("[");
-						loc_result.append(weekStrArr[Integer.parseInt(timeWeekTmp)]);
-						loc_result.append("]");
-					}
-					timeStartTmp = timpTmp.getString("beginTime");
-					if (StringUtils.isNotBlank(timeStartTmp)) {
-						loc_result.append(timeStartTmp);
-					}
-					timeEndTmp = timpTmp.getString("endTime");
-					if (StringUtils.isNotBlank(timeEndTmp) && timeEndTmp.equals(timeStartTmp) == false) {
-						loc_result.append("-");
-						loc_result.append(timeEndTmp);
+				if(timeArrTmp != null){
+					Iterator<Object> timeArrIterator = timeArrTmp.iterator();
+					JSONObject timpTmp = null;
+					String timeStartTmp = null;
+					String timeWeekTmp = null;
+					String timeEndTmp = null;
+					while (timeArrIterator.hasNext()) {
+						timpTmp = (JSONObject)timeArrIterator.next();
+						
+						loc_result.append(" ");
+						timeWeekTmp = timpTmp.getString("week");
+						if (StringUtils.isNotBlank(timeWeekTmp)) {
+							loc_result.append("[");
+							loc_result.append(weekStrArr[Integer.parseInt(timeWeekTmp)]);
+							loc_result.append("]");
+						}
+						timeStartTmp = timpTmp.getString("beginTime");
+						if (StringUtils.isNotBlank(timeStartTmp)) {
+							loc_result.append(timeStartTmp);
+						}
+						timeEndTmp = timpTmp.getString("endTime");
+						if (StringUtils.isNotBlank(timeEndTmp) && timeEndTmp.equals(timeStartTmp) == false) {
+							loc_result.append("-");
+							loc_result.append(timeEndTmp);
+						}
 					}
 				}
 			}
